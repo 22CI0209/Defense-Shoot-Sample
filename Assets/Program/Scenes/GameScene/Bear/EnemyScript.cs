aa957b47c20177223075c8caba814a5a9d141a98
22ci0212 <hypixel.block@gmail.com>
2023-06-14 16:30:58 +0900

    簡単に攻撃アニメーションの追加と社に当たっているか否かのステート処理を実装。 ダメージを受ける機能も実装したのでインスタンスから.Damage(ダメージ量)を呼べば体力が切れた際に破壊される

--- Assets/Program/Scenes/GameScene/Bear/EnemyScript.cs
+++ Assets/Program/Scenes/GameScene/Bear/EnemyScript.cs
@@ -4,18 +4,34 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-    //キャッシュ
-    RectTransform rect;
-    //マネージャーのインスタンス
-    EnemyManager manager;
+    RectTransform rect;     //キャッシュ
+    EnemyManager manager;   //マネージャーのインスタンス
 
+    enum State { Move, Attack, CoolDown };
+
+    [Header("アタッチ欄")]
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] Animator animator;
+
+    [Header("ステータス")]
+    [SerializeField] State state = State.Move;
+    [SerializeField] float hp = 10.0f;
+    //[SerializeField] float attack = 5.0f;
+    [SerializeField] bool isHit = false;
+    [Header("移動関係")]
     [SerializeField] Vector2 direction = Vector2.left;
     [SerializeField] float speed = 20.0f;
 
     public void SetManager(EnemyManager manager_)
     {
         manager = manager_;
+    }
+
+    public void Damage(float damage_)
+    {
+        hp -= damage_;
+        if (hp <= 0)
+            Destroy(gameObject);
     }
 
     void Start()
@@ -25,11 +41,38 @@ public class EnemyScript : MonoBehaviour
 
     void Update()
     {
-        rb.velocity = direction * speed;
+        switch (state)
+        {
+            case State.Move:
+                rb.velocity = direction * speed;
+                if (isHit)
+                    state = State.Attack;
+                break;
+            case State.Attack:
+                animator.SetTrigger("Attack");
+                state = State.CoolDown;
+                break;
+            case State.CoolDown:
+                break;
+        }
     }
 
     void OnDestroy()
     {
         manager.EnemysList.Remove(this);
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Castle")
+        {
+            isHit = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Castle")
+        {
+            isHit = false;
+        }
     }
 }
